0e2c0a707a14316ba6b9f06d96d305c509be229d
potatobucket <104957727+potatobucket@users.noreply.github.com>
2023-07-15 08:48:45 -0500

    Cell generation with random alive status working
    It works! I did it! (With a lot of help from my brother)

--- cell.py
+++ cell.py
@@ -5,14 +5,38 @@
 #-- Position on grid (x, y) (Maybe posX and posY?)
 #-- Number of neighbors
 
+import random as rndm
+import numpy as np
+
 class Cell:
-    def __init__(self, alive, posX, posY, neighbors):
+    def __init__(self, alive):
         self.alive = alive
-        self.posX = posX
-        self.posY = posY
-        self.neighbors = neighbors
     def life_status(self):
         if self.alive == False:
             return "The cell is dead."
         else:
             return "The cell is alive."
+
+testArray = np.full(shape = (2, 4), fill_value = "")
+testArray = testArray.tolist()
+# testArray = np.copy(testArray)
+
+states = [True, False]
+
+# for i in range(9):
+#     print(rndm.choice(states))
+
+rows = len(testArray)
+columns = len(testArray[0])
+
+for i in range(rows):
+    for j in range(columns):
+        testArray[i][j] = Cell(rndm.choice([True, False]))
+
+# for i in range(rows):
+#     for j in range(columns):
+#         testArray[i][j].alive = rndm.choice(states)
+
+for a in range(rows):
+    for b in range(columns):
+        print(testArray[a][b].alive)
