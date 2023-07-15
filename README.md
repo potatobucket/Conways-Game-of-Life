0284b3a09cec27adedb6ed27a674af8f9b33ef56
potatobucket <104957727+potatobucket@users.noreply.github.com>
2023-07-15 09:28:23 -0500

    Commenting for clarity
    Added some comments to clarify why I'm doing certain things in the code. Also updated the README.

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Conways-Game-of-Life
 Conway's Game of Life in Python
 
-I decided to take a crack at John Conway's Game of Life in Python.
+I decided to take a crack at John Conway's Game of Life in Python. Currently a work in-progress.
 
 Libraries used:
  - NumPy
+ - random
--- cell.py
+++ cell.py
@@ -11,16 +11,22 @@ class Cell:
         else:
             return "The cell is alive."
 
+
+#-- if you try to fill the array with cells at the outset it fills it with identical cells
+#-- so you have to create an empty array and then change all the elements to cells later
 testArray = np.full(shape = (2, 4), fill_value = "")
 testArray = testArray.tolist()
 
+#-- gets the size of the created array in both dimensions
 rows = len(testArray)
 columns = len(testArray[0])
 
+#-- changes all the empty elements to cells
 for i in range(rows):
     for j in range(columns):
         testArray[i][j] = Cell()
 
+#-- this is just for debugging purposes
 for a in range(rows):
     for b in range(columns):
         print(testArray[a][b].alive)
