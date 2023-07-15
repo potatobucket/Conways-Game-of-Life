c4c93945b02fef481bbc524493e820993b434403
potatobucket <104957727+potatobucket@users.noreply.github.com>
2023-07-15 08:54:42 -0500

    Cleaned up code
    Cleaned up all the stuff that didn't work. I left most of the test code just in case.

--- cell.py
+++ cell.py
@@ -1,16 +1,10 @@
-#-- this will be a class for cells
-#-- I need to think of what data it needs to keep track of
-#-- 
-#-- Life status (alive/dead)
-#-- Position on grid (x, y) (Maybe posX and posY?)
-#-- Number of neighbors
-
 import random as rndm
 import numpy as np
 
+#-- creates a cell object that is randomly alive or dead
 class Cell:
-    def __init__(self, alive):
-        self.alive = alive
+    def __init__(self):
+        self.alive = rndm.choice([True, False])
     def life_status(self):
         if self.alive == False:
             return "The cell is dead."
@@ -19,23 +13,13 @@ class Cell:
 
 testArray = np.full(shape = (2, 4), fill_value = "")
 testArray = testArray.tolist()
-# testArray = np.copy(testArray)
-
-states = [True, False]
-
-# for i in range(9):
-#     print(rndm.choice(states))
 
 rows = len(testArray)
 columns = len(testArray[0])
 
 for i in range(rows):
     for j in range(columns):
-        testArray[i][j] = Cell(rndm.choice([True, False]))
-
-# for i in range(rows):
-#     for j in range(columns):
-#         testArray[i][j].alive = rndm.choice(states)
+        testArray[i][j] = Cell()
 
 for a in range(rows):
     for b in range(columns):
