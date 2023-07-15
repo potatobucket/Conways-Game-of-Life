535094d041831355f70834f35d7278eb12c16ca0
potatobucket <104957727+potatobucket@users.noreply.github.com>
2023-07-14 21:29:12 -0500

    Modular grid is working
    It took a second but I got there!

--- grid_handling.py
+++ grid_handling.py
@@ -1,30 +1,26 @@
 import numpy as np
 
-grid = [
-    ["0", "0", "0", "0"],
-    ["0", "0", "0", "0"],
-    ["0", "0", "0", "0"],
-    ["0", "0", "0", "0"]
-]
+rows = int(input("How many rows?\n"))
+columns = int(input("How many columns?\n"))
 
-testArray = np.full(shape = (4,4), fill_value = "butts")
-print(testArray)
+grid = np.full(shape = (rows,columns), fill_value = "0")
+newGrid = np.copy(grid)
 
 horizontal = int(input("X coordinate?\n"))
 verticle = int(input("Y coordinate?\n"))
 
 def grid_handling():
-    for i in range(4):
-        for j in range(4):
+    for i in range(rows):
+        for j in range(columns):
             if i == verticle and j == horizontal:
-                grid[i][j] = "1"
+                newGrid[i][j] = "1"
             if i == verticle + 1 and j == horizontal + 1 or i == verticle - 1 and j == horizontal - 1:
-                grid[i][j] = "*"
+                newGrid[i][j] = "*"
             if i == verticle - 1 and j == horizontal + 1 or i == verticle + 1 and j == horizontal - 1:
-                grid[i][j] = "*"
+                newGrid[i][j] = "*"
             if i == verticle and j == horizontal + 1 or i == verticle + 1 and j == horizontal:
-                grid[i][j] = "*"
+                newGrid[i][j] = "*"
             if i == verticle - 1 and j == horizontal or i == verticle and j == horizontal - 1:
-                grid[i][j] = "*"
-    for square in grid:
+                newGrid[i][j] = "*"
+    for square in newGrid:
         print(square)
