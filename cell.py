db2a4f9faa885707bfd484e61d5a8d497337db0e
potatobucket <104957727+potatobucket@users.noreply.github.com>
2023-07-14 23:11:05 -0500

    Setup (hopefully) for cell class
    So's that I can populate the grid with objects instead of "0s."

---
+++ cell.py
@@ -0,0 +1,6 @@
+#-- this will be a class for cells
+#-- I need to think of what data it needs to keep track of
+#-- 
+#-- Life status (alive/dead)
+#-- Position on grid (x, y) (Maybe posX and posY?)
+#-- Number of neighbors
