dd9a9d3e6f26267ef446cdc5e54dd7460fa65be9
potatobucket <104957727+potatobucket@users.noreply.github.com>
2023-07-17 15:47:52 -0500

    I started over with neighbor detection
    I've gotten it where you can check the middle square of a 3x3 square and it will give you an accurate count of neighbors (+1 if the center square is alive).

--- debug.py
+++ debug.py
@@ -1,67 +1,17 @@
-import numpy as np
-import cell
+import grid_handling as gh
 
-testArray = np.full(shape = (3, 3), fill_value = "x, y")
-testArray = testArray.tolist()
+gh.populate_grid_with_cells()
+gh.get_visual_data_for_cells()
 
-rows = len(testArray)
-columns = len(testArray[0])
+# detectArea = [
+#     [(-1, -1), (-1, 0), (-1, 1)],
+#     [(0, -1), (0, 0), (0, 1)],
+#     [(1, -1), (1, 0), (1, 1)]
+# ]
 
-aliveArray = np.full(shape = (rows, columns), fill_value = "")
-aliveArray = aliveArray.tolist()
+for y in range(-1, 2):
+    for z in range(-1, 2):
+        if gh.cellGrid[1 - z][1 - y].alive == True:
+            gh.cellGrid[1][1].neighbors += 1
 
-coordinateArray = np.full(shape = (rows, columns), fill_value = "")
-coordinateArray = coordinateArray.tolist()
-
-for a in range(rows):
-    for b in range(columns):
-        testArray[a][b] = cell.Cell()
-
-visualArray = np.full(shape = (rows, columns), fill_value = "")
-visualArray = visualArray.tolist()
-for g in range(rows):
-    for h in range(columns):
-        visualArray[g][h] = testArray[g][h].visual
-
-for e in range(rows):
-    for f in range(columns):
-        aliveArray[e][f] = testArray[e][f].alive
-
-for c in range(rows):
-    for d in range(columns):
-        testArray[c][d].coordinateX = d
-        testArray[c][d].coordinateY = c
-
-for k in range(rows):
-    for l in range(columns):
-        coordinateArray[k][l] = f"({testArray[k][l].coordinateX}, {testArray[k][l].coordinateY})"
-
-for y in range(rows):
-    print(visualArray[y], coordinateArray[y])
-
-testX = int(input("Which X ya like to test, guvnah?\n"))
-testY = int(input("Which Y ya like to test, guvnah?\n"))
-
-def countNeighbors(grid, x, y):
-    sum = 0
-    for i in range(-1, 2):
-        for j in range(-1, 2):
-            if testArray[x + i][y + j].alive == True:
-                sum +=1
-    sum -= 1
-    return sum
-
-print(countNeighbors(testArray, testX, testY))
-
-# print("Number of neighbors: ", testArray[testX][testY].neighbors)
-
-# for poop in range(rows):
-#     print(aliveArray[poop])
-
-
-# for doodle in range(rows):
-#     for daddle in range(columns):
-#         print(testArray[doodle][daddle].coordinateX, testArray[doodle][daddle].coordinateY, testArray[doodle][daddle].alive)
-
-# for deedle in range(rows):
-#     print(visualArray[deedle], coordinateArray[deedle])
+print(f"Number of neighbors: {gh.cellGrid[1][1].neighbors}")
