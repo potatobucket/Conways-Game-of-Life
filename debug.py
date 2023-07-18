ddc228cd44ef89a1a7e7ac437be93e592be25406
potatobucket <104957727+potatobucket@users.noreply.github.com>
2023-07-17 19:59:33 -0500

    Cells don't count themselves as alive neighbors anymore
    You heard the man.

--- debug.py
+++ debug.py
@@ -13,9 +13,8 @@ for w in range(1, gh.rows - 1):
     for v in range(1, gh.columns - 1):
         for y in range(-1, 2):
             for z in range(-1, 2):
-                if gh.cellGrid[w + z][v + y].alive == True:
+                if gh.cellGrid[w + z][v + y].alive == True and gh.cellGrid[w + z][v + y] != gh.cellGrid[w][v]:
                     gh.cellGrid[w][v].neighbors += 1
-
 for smick in range(1, gh.rows - 1):
     for smack in range(1, gh.columns - 1):
         print(f"Number of neighbors for {smick, smack}: {gh.cellGrid[smick][smack].neighbors}")
