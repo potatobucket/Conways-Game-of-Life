ea7517357120c301ab583a39f4547e81575933ac
potatobucket <104957727+potatobucket@users.noreply.github.com>
2023-07-17 19:19:25 -0500

    I can check neighbors!
    Make like a tree and get outta here!

--- debug.py
+++ debug.py
@@ -9,9 +9,13 @@ gh.get_visual_data_for_cells()
 #     [(1, -1), (1, 0), (1, 1)]
 # ]
 
-for y in range(-1, 2):
-    for z in range(-1, 2):
-        if gh.cellGrid[1 - z][1 - y].alive == True:
-            gh.cellGrid[1][1].neighbors += 1
+for w in range(1, gh.rows - 1):
+    for v in range(1, gh.columns - 1):
+        for y in range(-1, 2):
+            for z in range(-1, 2):
+                if gh.cellGrid[w + z][v + y].alive == True:
+                    gh.cellGrid[w][v].neighbors += 1
 
-print(f"Number of neighbors: {gh.cellGrid[1][1].neighbors}")
+for smick in range(1, gh.rows - 1):
+    for smack in range(1, gh.columns - 1):
+        print(f"Number of neighbors for {smick, smack}: {gh.cellGrid[smick][smack].neighbors}")
